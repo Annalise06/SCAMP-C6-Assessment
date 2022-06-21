53766f5e9c34c8ce629d443e7ff70b6cfbc009d0
Anna Miene <tarerie00annie@gmail.com>
2022-06-21 19:26:43 +0100

    added new section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import sneaker from "../components/sneaker.jpg";
 
 
 const Hero = () => {
   return(
-    <div>
-
+    <div className="flex flex-col justify-between items-center relative container md:flex-row mx-auto space-y-0 md:space-y-4 ">
+      <div className="mx-4 my-8 md:w-1/2 ">
+      <img className='md:ml-6 shadow-lg rounded-xl' src={sneaker} alt="sneakers.jpg" size={10}/>
+      </div>
+      <div className="sm:ml-10">
+        <div className="ml-10 space-y-4">
+          <div className="text-sm text-orange-400 leading-6 font-bold space-y-1">
+            <h3>SNEAKERS COMPANY</h3>
+          </div>
+          <p className="font-bold text-5xl">Fall Limited Edition Sneakers</p>
+          <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer. </p>
+        </div>
+      </div>
+      <div>
+        
+      </div>
     </div>
   ) 
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,12 @@ const Navbar = () => {
   };
 
   return(
-    <div className="flex justify-between items-center text-black relative container mx-auto p-8 border-b-2">
+    <div className="flex justify-between items-center text-black sticky top-0 container mx-auto p-8 border-b-2">
       <div className="block md:hidden mx-4 mt-2 items-center " onClick={handleNav} >
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} /> }
       </div>
       
-        <h1 className="w-full text-3xl font-kanit font-bold">sneakers</h1>
+        <h1 className="w-full decoration-4 text-3xl  font-kanit font-bold">sneakers</h1>
         <ul className="hidden md:flex">
           <li className="p-4 hover:text-amber-400">Collections</li>
           <li className="p-4 hover:text-amber-400">Men</li>
@@ -24,13 +24,13 @@ const Navbar = () => {
           <li className="p-4 hover:text-amber-400">About</li>
           <li className="p-4 hover:text-amber-400">Contacts</li>
         </ul>
-        <div className="flex justify-between items-center mx-auto">
+        <div className="flex justify-between items-center relative mx-auto">
           <AiOutlineShoppingCart className="mt-2 mr-8" size={25} />
           <Avatar className="table-avatar mt-2" src="" />
         </div>
 
         <div className={!nav ? "fixed left-0 top-0 w-[60%] h-full pl-8 mx-20 ease-in-out duration-300" : "fixed left-[-100%]"}>
-        <ul className="pt-24  mt-4 uppercase">
+        <ul className="md:hidden pt-24  mt-4 uppercase">
           <li className="p-4 underline hover:text-amber-400">Collections</li>
           <li className="p-4 underline hover:text-amber-400">Men</li>
           <li className="p-4 underline hover:text-amber-400">Women</li>
